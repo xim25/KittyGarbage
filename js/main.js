1d8d48721f218e2a61760538336ccdb6f0e7afda
Ximena Perez <ximxim25@gmail.com>
2018-09-13 20:57:17 -0500

    Aparecen elementos random para recoger

--- js/main.js
+++ js/main.js
@@ -1,5 +1,66 @@
+//Variables
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
+
+
+
+var frames = 0;
+var score = [];
+
+var garbage = { 
+  apple: './img/apple.png',
+  banana: './img/banana.png',
+  chicken: './img/chicken.png',
+  bottle: './img/bottle.png',
+  cup: './img/cup.png',
+  box: './img/box.png'
+};
+
+// var enemies = {
+//   dog: './img/dog.png',
+//   cucumber: './img/cucumber.png',
+//   ice: './img/ice.png',
+//   shower: './img/shower.png',
+// }
+
+var audios = {
+  back: './audio/back.mp3',
+  win: './audio/dientes.mp3',
+  organic: './audio/organic.mp3',
+  inorganic: './audio/inorganic.mp3'
+};
+
+//Classes ****Faltan animaciones****
+class Office{
+
+  constructor(){
+    this.x = 0;
+    this.y = 0;
+    this.width = canvas.width;
+    this.height = canvas.height;
+    this.image = new Image();
+    this.image.src = "./img/Fondo.png";
+    this.score = 0;
+    }
+  
+
+  draw(){
+    this.x--;
+        if(this.x < -canvas.width) this.x = 0;
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+        ctx.globalAlpha = 0.9;
+        ctx.fillStyle = 'black';
+        ctx.fillRect(0,0,canvas.width,60);
+        ctx.globalAlpha = 1.0;
+        ctx.font = '20px basis-grotesque-mono';
+        ctx.fillStyle = 'white';
+        ctx.fillText(`Time: ${Math.round(frames/60)}`, 50, 37);
+        ctx.fillText('Score: '+ this.score, 200, 37);
+        ctx.fillText('Lives: '+ this.deadliItems, 500, 37); //Arreglar display
+        
+  }
+}
 
 class Kitty{
 
@@ -10,61 +71,224 @@ class Kitty{
     this.height = 77;
     this.image = new Image();
     this.image.src = "./img/kitty.png";
+    this.deadliItems = 3;
   }
 
-  // collision(item){
-  //   return (this.x < item.x + item.width) &&
-  //          (this.x + this.width > item.x) &&
-  //          (this.y < item.y + item.height) &&
-  //          (this.y + this.height > item.y);
-  // }
+  collision(item){
+    return (this.x < item.x + item.width) &&
+        (this.x + this.width > item.x) &&
+        (this.y < item.y + item.height) &&
+        (this.y + this.height > item.y);
+}
 
   draw(){
-    if(this.y < 490) this.y += 5;
+        if(this.y < 490) this.y += 4;
         if(frames % 10 === 0 ) this.image;
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
   }
 }
 
+class Apple{
 
-class Office{
-
-  constructor(){
-    this.x = 0;
-    this.y = 0;
-    this.width = canvas.width;
-    this.height = canvas.height;
+  constructor(y){
+    this.x = canvas.width;
+    this.y = y;
+    this.width = 50;
+    this.height = 57;
     this.image = new Image();
-    this.image.src = "./img/Fondo.png";
+    this.image.src = garbage.apple;
+    this.type = "organic";
   }
 
   draw(){
-    
-    if(this.x < -canvas.width) this.x = 0;
-    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-    ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+    if(frames % 10 === 0) this.x -= 15;
+    ctx.drawImage(this.image,this.x,this.y,this.width,this.height)
+  }
+}
+
+class Banana{
+
+  constructor(y){
+    this.x = canvas.width;
+    this.y = y;
+    this.width = 75.5;
+    this.height = 62;
+    this.image = new Image();
+    this.image.src = garbage.banana;
+    this.type = "organic";
   }
 
+  draw(){
+    if(frames % 10 === 0) this.x -= 15;
+    ctx.drawImage(this.image,this.x,this.y,this.width,this.height)
+  }
 }
 
 
-var kitty = new Kitty();
+class Chicken{
+
+  constructor(y){
+    this.x = canvas.width;
+    this.y = y;
+    this.width = 80;
+    this.height = 80;
+    this.image = new Image();
+    this.image.src = garbage.chicken;
+    this.type = "organic";
+  }
+
+  draw(){
+    if(frames % 10 === 0) this.x -= 15;
+    ctx.drawImage(this.image,this.x,this.y,this.width,this.height)
+  }
+}
+
+
+
+
+class Bottle{
+
+  constructor(y){
+    this.x = canvas.width;
+    this.y = y;
+    this.width = 27;
+    this.height = 80;
+    this.image = new Image();
+    this.image.src = garbage.bottle;
+    this.type = "inorganic";
+  }
+
+  draw(){
+    if(frames % 10 === 0) this.x -= 25;
+    ctx.drawImage(this.image,this.x,this.y,this.width,this.height)
+  }
+}
+
+
+class Cup{
+
+  constructor(y){
+    this.x = canvas.width;
+    this.y = y;
+    this.width = 50;
+    this.height = 55;
+    this.image = new Image();
+    this.image.src = garbage.cup;
+    this.type = "inorganic"
+  }
+
+  draw(){
+    if(frames % 10 === 0) this.x -= 25;
+    ctx.drawImage(this.image,this.x,this.y,this.width,this.height)
+  }
+}
+
+class Box{
+
+  constructor(y){
+    this.x = canvas.width;
+    this.y = y;
+    this.width = 62;
+    this.height = 96.5;
+    this.image = new Image();
+    this.image.src = garbage.box;
+    this.type = "inorganic";
+  }
+
+  draw(){
+    if(frames % 10 === 0) this.x -= 25;
+    ctx.drawImage(this.image,this.x,this.y,this.width,this.height)
+  }
+}
+
+
 var office = new Office();
+var kitty = new Kitty();
 
 
-var frames = 0;
 var interval = setInterval(function(){
     frames++;
     office.draw();
     kitty.draw();
-    
+    generateElements();
+    drawElements();
 }, 1000/60);
 
 
+//Functions
+
+var elements = [];
+
+var items = ["apple", "bottle", "banana", "chicken", "cup", "box"];
+
+function generateElements(){
+  if(frames % 50 === 0 || frames % 500 === 0 || frames % 1000 === 0){
+    let altura = Math.floor((Math.random() *  400) + 100 );
+    let type = items[Math.floor((Math.random() * 6))]
+    let item;
+
+    // completar los cases
+    switch (type) {
+      case "apple":
+        item = new Apple(altura);
+        break;
+
+      case "bottle":
+        item = new Bottle(altura);
+        break
+
+      case "banana":
+        item = new Banana(altura);
+        break
+
+      case "chicken":
+        item = new Chicken(altura);
+        break
+
+      case "cup":
+        item = new Cup(altura);
+        break
+
+      case "box":
+        item = new Box(altura);
+        break
+
+    
+      default:
+        return;
+        break;
+    }
+    elements.push(item);
+  }
+}
+
+function drawElements(){
+  elements.forEach(function(elem, index){
+      elem.draw();
+      if(kitty.collision(elem)){
+        if(elem.type === "organic") office.score += 1;
+        if(elem.type === "inorganic"){
+          if(kitty.deadliItems < 0) clearInterval(interval);
+          kitty.deadliItems -= 1;
+        }
+        elements.splice(index, 1);
+          if(kitty.deadliItems < 0) {
+            ctx.font = '80px basis-grotesque-mono';
+            ctx.fillStyle = 'white';
+            ctx.fillText("Bye");
+          }
+      };
+      //Función game over
+      })
+}
+
+
+
+//Events
 addEventListener("keydown", function(e){
-  if(e.keyCode === 32 && kitty.x <= canvas.width - kitty.width*4){
-      kitty.y -= 100;
-      kitty.x += kitty.width;
+  if(e.keyCode === 32){
+      kitty.y -= 150;
+      // if(kitty.x <= canvas.width - kitty.width*4) kitty.x += kitty.width;
   }
 
   if(e.keyCode === 39 && kitty.x < canvas.width - kitty.width*4){
